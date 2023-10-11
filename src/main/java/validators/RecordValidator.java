9a251263eae5f0bc0c2d5d1da2c9453c9c68be97
sevtorushin <vtorushin1991@mail.ru>
2023-10-11 18:51:25 +0900

    Создан класс RecordValidator

--- src/main/java/validators/RecordValidator.java
+++ src/main/java/validators/RecordValidator.java
@@ -1,21 +1,45 @@
 package validators;
 
+import parsers.RecordParser;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.List;
+import java.util.Set;
 
-public class RecordValidator implements Validator{
-    private String packageNumber;
-    private List<String> itemList;
+public class RecordValidator implements Validator {
+    private final String packageNumber;
+    private final Set<String> itemSet;
 
-    public RecordValidator() {
+    public RecordValidator(String packageNumber, Set<String> itemSet) {
+        this.packageNumber = packageNumber;
+        this.itemSet = itemSet;
     }
 
     @Override
     public boolean isValid(String data) {
-        return false;
+        String packageNumber = RecordParser.getPackageNumber(data);
+        String item = RecordParser.getItem(data);
+        String value = RecordParser.getValue(data);
+        boolean isValidPackageNumber = packageNumber.equals(this.packageNumber);
+        boolean isValidItem = itemSet.contains(item);
+        boolean isValidValue;
+        switch (item) {
+            case "01":
+                isValidValue = true;
+                break;
+            case "05":
+                isValidValue = isDate(value);
+                break;
+            case "06":
+                isValidValue = isTime(value);
+                break;
+            default:
+                isValidValue = isNumber(value);
+                break;
+        }
+        return isValidPackageNumber && isValidItem && isValidValue;
     }
 
     private boolean isNumber(String value) {
