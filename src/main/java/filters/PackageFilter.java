009284a7adeab026d6bd8d5375790d8edcee347f
sevtorushin <vtorushin1991@mail.ru>
2023-10-30 15:30:05 +0900

    Добавлен фунционал добавления и удаления номеров пакетов для фильтрации

--- src/main/java/filters/PackageFilter.java
+++ src/main/java/filters/PackageFilter.java
@@ -4,16 +4,17 @@ import exceptions.WitsParseException;
 import parsers.WitsPackageParser;
 import parsers.WitsParsersProvider;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.Set;
 
 public class PackageFilter implements Filter {
 
-    private List<String> packageNumbers;
+    private Set<String> packageNumbers;
     private WitsParsersProvider packageProvider;
 
-    public PackageFilter(WitsParsersProvider packageProvider, String... packageNumbers) {
-        this.packageNumbers = List.of(packageNumbers);
-        this.packageProvider = packageProvider;
+    public PackageFilter(String... packageNumbers) {
+        this.packageNumbers = Set.of(packageNumbers);
+        this.packageProvider = new WitsParsersProvider();
     }
 
     public String filtrate(String witsPackage) {
@@ -27,5 +28,37 @@ public class PackageFilter implements Filter {
             return null;
         }
         return witsPackage;
+    }
+
+    public void addPackageNumber(String packageNumber) {
+        this.packageNumbers.add(packageNumber);
+    }
+
+    public void addPackageNumbers(String... packageNumbers) {
+        this.packageNumbers.addAll(Arrays.asList(packageNumbers));
+    }
+
+    public void removePackageNumber(String packageNumber) {
+        this.packageNumbers.remove(packageNumber);
+    }
+
+    public void removePackageNumbers(String... packageNumbers) {
+        this.packageNumbers.removeAll(Arrays.asList(packageNumbers));
+    }
+
+    public Set<String> getPackageNumbers() {
+        return packageNumbers;
+    }
+
+    public void setPackageNumbers(Set<String> packageNumbers) {
+        this.packageNumbers = packageNumbers;
+    }
+
+    public WitsParsersProvider getPackageProvider() {
+        return packageProvider;
+    }
+
+    public void setPackageProvider(WitsParsersProvider packageProvider) {
+        this.packageProvider = packageProvider;
     }
 }
