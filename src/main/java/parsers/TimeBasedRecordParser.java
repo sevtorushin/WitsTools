416542b4905de62b0baa39e3305778727cd8146d
sevtorushin <vtorushin1991@mail.ru>
2023-11-05 16:39:55 +0900

    Добавлена документация к классу TimeBasedRecordParser. Удалены неиспользуемые зависимости из pom

--- src/main/java/parsers/TimeBasedRecordParser.java
+++ src/main/java/parsers/TimeBasedRecordParser.java
@@ -7,11 +7,31 @@ import validators.ValidatorBuilder;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *The {@code TimeBasedRecordParser} class allows you to parse individual records in WITS packages, dividing each record into package number, item and value.
+ * <p>
+ * {@code TimeBasedRecordParser} comes with a {@code TimeBasedRecordValidator} record validator that validates each record according to {@code TimeBasedDescription} - a WITS packet description based on the Wellsite transfer specification. Optionally, record validation can be disabled using the {@code resetValidation()} method.
+ *
+ * @apiNote
+ * {@code TimeBasedRecordParser} extends the functionality of the {@code WitsRecordParser} class.
+ *
+ * @author Sergey Vorushin
+ * @see WitsRecordParser
+ */
+
 public class TimeBasedRecordParser extends WitsRecordParser {
+
+    /**
+     * The default constructor initializes the parser object {@code TimeBasedRecordParser} with the default validator {@code TimeBasedRecordValidator}.
+     */
     public TimeBasedRecordParser() {
         super(new ValidatorBuilder<>(new ArrayList<>(List.of(new TimeBasedRecordValidator()))), new RecordSplitter(2, 2));
     }
 
+    /**
+     * The extended constructor allows you to initialize the parser with your own validator, which is a descendant of {@code TimeBasedRecordValidator}.
+     * @param validatorBuilder builder, which accepts several validators - descendants of {@code TimeBasedRecordValidator}.
+     */
     public TimeBasedRecordParser(ValidatorBuilder<TimeBasedRecordValidator> validatorBuilder) {
         super(validatorBuilder, new RecordSplitter(2, 2));
     }
